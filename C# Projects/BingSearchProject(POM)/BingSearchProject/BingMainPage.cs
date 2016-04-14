0fda104a43c96002a3363d38cd22259cdcbe8058
Oleksiy Holyarchuk <oleksiy.holyarchuk@gmail.com>
2016-04-14 21:08:18 +0300

    Bing search using POM.

---
+++ C# Projects/BingSearchProject(POM)/BingSearchProject/BingMainPage.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectPattern
+{
+    public class BingMainPage
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = @"http://www.bing.com/";
+
+        public BingMainPage(IWebDriver browser)
+        {
+            this.driver = browser;
+            this.driver.Manage().Window.Maximize();
+            PageFactory.InitElements(browser, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "sb_form_q")]
+        public IWebElement SearchBox { get; set; }
+
+        [FindsBy(How = How.Id, Using = "sb_form_go")]
+        public IWebElement GoButton { get; set; }
+
+        [FindsBy(How = How.Id, Using = "b_tween")]
+        public IWebElement ResultsCountDiv { get; set; }
+
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(this.url);
+        }
+
+        public void Search(string textToType)
+        {
+            this.SearchBox.Clear();
+            this.SearchBox.SendKeys(textToType);
+            this.GoButton.Click();
+        }
+
+        public void ValidateResultsCount(string expectedCount)
+        {
+
+            Assert.IsTrue(this.ResultsCountDiv.Text.Contains(expectedCount), "The results DIV doesn't contains the specified text.");
+        }
+    }
+}
---
+++ C# Projects/BingSearchProject(POM)/BingSearchProject/BingTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PageObjectPattern
+{
+    [TestClass]
+    public class BingTests
+    {
+        public IWebDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.Driver = new FirefoxDriver();
+            this.Wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(60));
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SearchTextInBing()
+        {
+            BingMainPage googleMainPage = new BingMainPage(this.Driver);
+            googleMainPage.Navigate();
+            googleMainPage.Search("автоматизоване тестування");
+            googleMainPage.ValidateResultsCount("7,380 РЕЗУЛЬТАТИ");
+        }
+    }
+}
