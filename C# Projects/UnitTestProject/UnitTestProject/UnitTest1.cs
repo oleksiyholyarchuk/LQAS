c58f791b6ff5c6d6edecbf9cab32b60a6e299e44
Oleksiy Holyarchuk <oleksiy.holyarchuk@gmail.com>
2016-04-14 17:37:03 +0300

    My first C# Project file ever.

---
+++ C# Projects/UnitTestProject/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumExample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        FirefoxDriver firefox;
+
+        // это тест.
+        [TestMethod]
+        public void TestMethod1()
+        {
+            firefox = new FirefoxDriver();
+            firefox.Navigate().GoToUrl("https://github.com/login");
+            firefox.FindElement(By.Id("login_field")).SendKeys("oleksiyholyarchuk");
+            firefox.FindElement(By.Id("password")).SendKeys("********");
+            firefox.FindElement(By.Name("commit")).Click();
+        }
+
+        // разрушение объекта драйвера после окончание теста.
+        [TestCleanup]
+        public void TearDown()
+        {
+            firefox.Quit();
+        }
+    }
+}
